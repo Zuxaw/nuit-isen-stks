7277b66256e3d7f3e87438612ca2f1d92d059e8d
vecil <69813615+vecil@users.noreply.github.com>
2023-01-14 05:32:44 +0100

    ✨ update: fetch chambers from graphql

--- client/src/screens/rooms/Rooms.tsx
+++ client/src/screens/rooms/Rooms.tsx
@@ -1,90 +1,69 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from 'react-router-dom';
+import Error from '~/appkit/Error';
+import Loading from '~/appkit/Loading';
 
-import Step from "~/appkit/Step";
-import Header from "~/components/header/Header";
+import { useQuery } from '@apollo/client';
+import Step from '~/appkit/Step';
+import Header from '~/components/header/Header';
+import { getChambersQuery } from '~/queries/queries';
+
+export type ChamberProps = {
+  number: number;
+  typology: string;
+  description?: string;
+  pricing: number;
+  pictures?: string[];
+};
 
 const Rooms = () => {
   const navigate = useNavigate();
-  const { state } = useLocation();
-  console.log("state", state);
-  // fake carte chamber hotel data
-  const rooms = [
-    {
-      id: 1,
-      name: "Chambre 1",
-      price: 100,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 2,
-      name: "Chambre 2",
-      price: 200,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 3,
-      name: "Chambre 3",
-      price: 300,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 4,
-      name: "Chambre 4",
-      price: 400,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 5,
-      name: "Chambre 5",
-      price: 500,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 6,
-      name: "Chambre 6",
-      price: 600,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 7,
-      name: "Chambre 7",
-      price: 700,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 8,
-      name: "Chambre 8",
-      price: 800,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 9,
-      name: "Chambre 9",
-      price: 900,
-      images: ["https://picsum.photos/200/300"],
-    },
-    {
-      id: 10,
-      name: "Chambre 10",
-      price: 1000,
-      images: ["https://picsum.photos/200/300"],
-    },
-  ];
+  const { state }: any = useLocation();
+
+  const adults = state?.adults;
+  const startDate = state?.startDate;
+  const endDate = state?.endDate;
+  console.log('state', state);
+
+  const goToRoomDetails = (room: ChamberProps) => {
+    navigate('/roomsdetail', {
+      state: {
+        adults,
+        startDate,
+        endDate,
+        room,
+      },
+    });
+  };
+
+  const { loading, error, data } = useQuery(getChambersQuery);
+  if (loading) return <Loading />;
+  if (error) return <Error error={error.message} />;
+
+  const rooms: ChamberProps[] = data.chambers;
+  console.warn(rooms.length);
+
+  if (rooms.length === 0) return <Error error={'No chambers found.'} />;
+
   return (
     <div>
       <Header />
       <Step step={2} />
       <div className="flex flex-wrap justify-center">
         {rooms.map((room) => (
-          <div className="card w-96 bg-base-100 shadow-xl m-5" key={room.id}>
+          <div className="card w-96 bg-base-100 shadow-xl m-5" key={room.number}>
             <figure>
-              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+              {room.pictures && room.pictures.length > 0 ? (
+                <img src={room.pictures[Math.floor(Math.random() * room.pictures.length)]} />
+              ) : (
+                <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+              )}
             </figure>
             <div className="card-body">
-              <h2 className="card-title">{room.name}</h2>
+              <h2 className="card-title">{room.typology}</h2>
+              <p>{room?.description}</p>
               <div className="card-actions justify-end">
-                <a onClick={() => navigate("/roomsdetail")}>
-                  <button className="btn btn-primary">{room.price} €</button>
+                <a onClick={() => goToRoomDetails(room)}>
+                  <button className="btn btn-primary">{room.pricing} €</button>
                 </a>
               </div>
             </div>
--- client/src/screens/rooms/RoomsDetail.tsx
+++ client/src/screens/rooms/RoomsDetail.tsx
@@ -1,69 +1,13 @@
-import Step from "~/appkit/Step";
-import Header from "~/components/header/Header";
+import { useLocation } from 'react-router-dom';
+import Step from '~/appkit/Step';
+import Header from '~/components/header/Header';
+import { ChamberProps } from './Rooms';
 
 const RoomsDetail = () => {
-    const rooms = [
-        {
-          id: 1,
-          name: "Chambre 1",
-          price: 100,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 2,
-          name: "Chambre 2",
-          price: 200,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 3,
-          name: "Chambre 3",
-          price: 300,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 4,
-          name: "Chambre 4",
-          price: 400,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 5,
-          name: "Chambre 5",
-          price: 500,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 6,
-          name: "Chambre 6",
-          price: 600,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 7,
-          name: "Chambre 7",
-          price: 700,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 8,
-          name: "Chambre 8",
-          price: 800,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 9,
-          name: "Chambre 9",
-          price: 900,
-          images: ["https://picsum.photos/200/300"],
-        },
-        {
-          id: 10,
-          name: "Chambre 10",
-          price: 1000,
-          images: ["https://picsum.photos/200/300"],
-        },
-      ];
+  const { state }: any = useLocation();
+  console.log(state);
+
+  const room: ChamberProps = state.room;
   return (
     <>
       <Header />
@@ -71,17 +15,14 @@ const RoomsDetail = () => {
         <Step step={2} />
         <div className="hero min-h-screen bg-base-300">
           <div className="hero-content flex-col lg:flex-row">
-            <img
-              src="https://placeimg.com/260/400/arch"
-              className="max-w-sm rounded-lg shadow-2xl"
-            />
+            {room.pictures && room.pictures.length > 0 ? (
+              <img src={room.pictures[Math.floor(Math.random() * room.pictures.length)]} />
+            ) : (
+              <img src="https://placeimg.com/400/225/arch" alt="Shoes" />
+            )}
             <div>
-              <h1 className="text-5xl font-bold">ROOM</h1>
-              <p className="py-9">
-                Provident cupiditate voluptatem et in. Quaerat fugiat ut
-                assumenda excepturi exercitationem quasi. In deleniti eaque aut
-                repudiandae et a id nisi.
-              </p>
+              <h1 className="text-5xl font-bold">{room.typology}</h1>
+              <p className="py-9">{room?.description}</p>
               <button className="btn btn-primary">Get Started</button>
             </div>
           </div>
